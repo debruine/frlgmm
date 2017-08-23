b3e7cea304e5e905da9f7b055053a3557a8d15cd
Lisa DeBruine <lisa.debruine@glasgow.ac.uk>
2017-08-23 11:22:40 +0100

    Fixed a bug in the broken-stick methods for selecting "significant" PCs.

--- R/selectPCs.R
+++ R/selectPCs.R
@@ -2,9 +2,9 @@
 #'
 #' \code{selectPCs} returns a list of significant PCs as chosen by one of 3 methods
 #' (from D.A. Jackson (1993). Ecology, 74, 2204-2214). Defaults to the most accurate
-#' and conservative "broken-stick" method. Other methods are "Kaiser-Guttman" (PCs
-#' with eigenvalues greater than the mean eigenvalue) and "total variance" (PCs
-#' explaining at least total.var variance)
+#' and conservative "broken-stick"/"bs" method. Other methods are "Kaiser-Guttman"/"kg"
+#' (PCs with eigenvalues greater than the mean eigenvalue) and "total variance"/"tv"
+#' (PCs explaining at least total.var variance)
 #'
 #' @param data Data structure from geomorph or created by readTem()
 #' @param method The method to use to choose PCs (Default "broken-stick")
@@ -17,24 +17,30 @@
 selectPCs <- function(data, method="broken-stick", total.var = .95) {
   if (is.null(data$pca$sdev)) { data$pca <- getPCA(data); }
 
-  ev <- data$pca$sdev^2;
+  ev <- data$pca$sdev^2
+  method <- tolower(method)
 
-  if (method == "broken-stick") {
+  if (method == "broken-stick" | method == "bs") {
     # compare variance explained to null model
-    p <- length(ev);
-    i <- rep(1:p);
-    b.k <- sapply(i, function(x) sum(1/i[x:p]));
-    var <- ev*100/sum(ev);
-    pc.end <- sum(var >= b.k);
-  } else if (method == "Kaiser-Guttman") {
+    n_ev <- length(ev)
+    bsm <- data.frame(
+      j = seq(1:n_ev),
+      p = 0
+    )
+    bsm$p[1] <- 1/n_ev
+    for (i in 2:n_ev) bsm$p[i] <- bsm$p[i-1] + (1/(n_ev + 1 - i))
+    bsm$p <- 100*bsm$p/n_ev
+    test <- cbind(100*ev/sum(ev), bsm$p[n_ev:1])
+    pc_end <- sum(test[,1] >= test[,2])
+  } else if (method == "kaiser-guttman" |  method == "kg") {
     # return PCs with eigenvalues greater than the mean eigenvalue
-    pc.end <- sum(ev >= mean(ev));
-  } else if (method == "total variance") {
+    pc_end <- sum(ev >= mean(ev));
+  } else if (method == "total variance" | method == "tv") {
     # return PCs explaining at least total.var variance
     cumvar <- cumsum(ev / sum(ev));
-    pc.end <- sum(cumvar < total.var) + 1;
+    pc_end <- sum(cumvar < total.var) + 1;
   }
 
-  pcs <- rep(1:pc.end)
+  pcs <- rep(1:pc_end)
   return(pcs);
 }
--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 ---
 title: "README"
 author: "Lisa DeBruine"
-date: "3 October 2015"
+date: "2017-08-23"
 output: html_document
 ---
 
-#Face Research Lab Geometric Morphometric Functions
+# Face Research Lab Geometric Morphometric Functions
 
-Convenience functions supporting the visualisation of stimuli delineated in Psychomorph, using Face Research Lab (facelab.org) defaults.
+Convenience functions supporting the visualisation of stimuli delineated in [WebMorph](http://webmorph.org) or Psychomorph, using [Face Research Lab](http://facelab.org) defaults.
 
-##Notes
+## Notes
 
-1. On a Mac, you'll probably need to install X11 from http://xquartz.macosforge.org and have this running anytime you want to run geomorph
+1. On a Mac, you'll need to install X11 from http://xquartz.macosforge.org before you install the `geomorph` package
 2. The package comes with a text dataset called `faces`
